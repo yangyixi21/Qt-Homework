20eaeb5ed1b14e012d0ca5048d734b279eb179b5
momona06 <93273462+momona06@users.noreply.github.com>
2022-05-24 22:54:10 +0800

    Add files via upload

---
+++ homeworkbig/mainwindow.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+
+    initGame();
+
+}
+
+MainWindow::~MainWindow()
+{
+
+
+}
+
+void MainWindow::initGame()
+{
+
+}
---
+++ homeworkbig/screen.h
@@ -0,0 +1,22 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+#include<QMainWindow>
+#include<QObject>
+#include<QEvent>
+#include<QPushButton>
+#include<QLabel>
+#include<vector>
+class screen{
+   // Q_OBJECT
+
+public:
+    std::vector<QPushButton> buttonvec;
+
+
+private:
+
+
+};
+
+
+#endif // SCREEN_H
